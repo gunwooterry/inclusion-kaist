cf8605e9cd1a67a8d37ccb17ff144b383545db08
Gunwoo Kim <gunwooterry@gmail.com>
2018-03-11 00:48:24 +0900

    NewsCard in Landing (error)

--- src/components/common/NewsCard.jsx
+++ src/components/common/NewsCard.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ryuImage from './../../static/ryu.png';
 
-
 function NewsCard(props) {
   const {
     title,
@@ -13,9 +12,9 @@ function NewsCard(props) {
 
   const styles = {
     boxStyle: {
-      border: '0.1rem solid #ededed',
+      border: '0.1rem solid #EDEDED',
       borderRadius: '0.1rem',
-      width: '30rem',
+      width: '100%',
       height: '12rem',
       margin: 'auto',
       textAlign: 'left',
@@ -46,7 +45,8 @@ function NewsCard(props) {
 
   return (
     <div style={styles.boxStyle}>
-      <img style={styles.imageStyle} src={ryuImage} alt="professor" />
+      <div style={styles.imageStyle} />
+      {/* <img style={styles.imageStyle} src={ryuImage} alt="professor" /> */}
       <div style={styles.contentStyle}>
         <h3 style={styles.titleStyle}>{title}</h3>
         <p style={styles.dateStyle}>{date}</p>
@@ -63,8 +63,7 @@ NewsCard.propTypes = {
 };
 
 NewsCard.defaultProps = {
-  title: '즐거운 크리스마스,\n' +
-  '과연 모두에게 즐거울까? ',
+  title: '즐거운 크리스마스, 과연 모두에게 즐거울까?',
   date: '2017-12-24',
   imagePath: './../../static/ryu.png',
   size: 1,
