6c95999e3a274f1d171c30134b09a9cf36b1d6a2
Gunwoo Kim <gunwooterry@gmail.com>
2018-03-30 09:36:31 +0900

    fix: remove unused components

--- client/src/components/common/OrgCard.jsx
+++ client/src/components/common/OrgCard.jsx
@@ -23,6 +23,7 @@ function OrgCard(props) {
       verticalAlign: 'top',
       padding: '19px 20px',
       boxShadow: '0 2px 6px rgba(0, 0, 0, 0.25)',
+      textAlign: 'left',
     },
     image: {
       width: '100%',
