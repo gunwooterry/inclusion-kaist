ee9bcc2920203dc75bd64e6ad1f8d317d3b0db3b
Gunwoo Kim <gunwooterry@gmail.com>
2018-03-30 09:44:10 +0900

    feat: redesign report
    Redesign the report page
    Compress the background image on the landing page

--- client/src/components/Report.jsx
+++ client/src/components/Report.jsx
@@ -1,28 +1,115 @@
 import React, { Component } from 'react';
 import Scroll from 'react-scroll';
+import Textarea from 'react-textarea-autosize';
 
-import ReportSelect from './ReportSelect';
-import ReportPhone from './ReportPhone';
-import ReportExplain from './ReportExplain';
-import ReportDone from './ReportDone';
+import { colors } from '../static/constants';
+import Button from './common/Button';
 import Column from './common/Column';
+import Header from './common/Header';
 import Row from './common/Row';
+import Section from './common/Section';
 
-const { Element, scroller, animateScroll } = Scroll;
-const half = window.innerHeight * 1.2 / 4;
-const lastQuater = (window.innerHeight * 2.2) / 4;
+const { Element, scroller } = Scroll;
+const half = window.innerHeight * 0.3;
+const lastQuater = window.innerHeight * 0.55;
+
+const SELECT = 'select';
+const EXPLAIN = 'explain';
+const PHONE = 'phone';
+
+const scrollToElement = (name) => {
+  scroller.scrollTo(name, {
+    duration: 500,
+    delay: 100,
+    smooth: true,
+    offset: -half,
+    ignoreCancelEvents: true,
+  });
+};
+
+const formatPhoneNumber = (phone) => {
+  const digits = phone.split('');
+  if (digits.length >= 11) {
+    digits.splice(7, 0, '-');
+  } else if (digits.length > 6) {
+    digits.splice(6, 0, '-');
+  }
+  if (digits.length > 2) {
+    digits.splice(3, 0, '-');
+  }
+  return digits.join('');
+};
 
 const styles = {
-  arrowStyle: {
-    width: '3rem',
-    position: 'absolute',
-    top: '50%',
+  centered: {
+    textAlign: 'center',
+  },
+  topSection: {
+    paddingTop: '10rem',
+    paddingBottom: '6rem',
+  },
+  section: {
+    padding: '4rem 0',
+  },
+  scrollContainer: {
+    scrollSnapType: 'y mandatory',
+  },
+  container: {
+    marginTop: '2rem',
+    marginBottom: '2rem',
+    overflow: 'auto',
+  },
+  unfocused: {
+    opacity: 0.2,
     cursor: 'pointer',
   },
-  divStyle: {
-    marginTop: '100px',
-    overflow: 'auto',
-    scrollSnapType: 'y mandatory',
+  option: {
+    width: '100%',
+    margin: '8px auto 8px',
+    backgroundColor: 'transparent',
+    borderWidth: 0,
+    textAlign: 'center',
+    verticalAlign: 'top',
+    padding: '25px 20px',
+    boxShadow: '0 2px 6px rgba(0, 0, 0, 0.25)',
+    outline: 'none',
+    cursor: 'pointer',
+  },
+  optionText: {
+    fontSize: '1.5rem',
+    fontWeight: 400,
+  },
+  selected: {
+    backgroundColor: colors.kaistBlue,
+    color: colors.white,
+    boxShadow: '0 2px 8px rgba(0, 0, 0, 0.25)',
+  },
+  textarea: {
+    borderColor: colors.kaistBlue,
+    borderWidth: 1,
+    padding: '0.8rem',
+    fontSize: '1.2rem',
+    fontWeight: 400,
+    lineHeight: '160%',
+    width: '90%',
+    maxWidth: 900,
+    outline: 'none',
+  },
+  paragraph: {
+    fontSize: '1.2rem',
+    fontWeight: 300,
+    lineHeight: '160%',
+  },
+  phoneInput: {
+    borderWidth: 0,
+    borderBottomColor: colors.kaistBlue,
+    borderBottomWidth: 1,
+    padding: '0.5rem',
+    fontSize: '1.5rem',
+    fontWeight: 400,
+    lineHeight: '160%',
+    width: 300,
+    outline: 'none',
   },
 };
 
@@ -30,15 +117,11 @@ class Report extends Component {
   constructor() {
     super();
     this.state = {
-      selectFocus: true,
-      explainFocus: false,
-      phoneFocus: false,
+      focus: SELECT,
+      reportType: -1,
+      phoneNumber: '',
     };
     this.handleScroll = this.handleScroll.bind(this);
-  }
-
-  onSubmit() {
-    // add http request
   }
 
   componentDidMount() {
@@ -49,88 +132,142 @@ class Report extends Component {
     window.removeEventListener('scroll', this.handleScroll);
   }
 
-  handleScroll(event) {
+  onSelectOption(event, id) {
+    if (this.state.focus === SELECT) {
+      event.stopPropagation();
+      this.setState({ reportType: id });
+      scrollToElement(EXPLAIN);
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  onSubmit() {
+    // add http request
+  }
+
+  handleScroll() {
+    const { focus } = this.state;
     const selectPos = this.selectNode.getBoundingClientRect();
-    // const explainPos = this.explainNode.getBoundingClientRect();
-    // let scrollPos = window.scrollY;
     const phonePos = this.phoneNode.getBoundingClientRect();
-    const selectFocus = selectPos.y > -selectPos.height / 3;
-    const phoneFocus = phonePos.y < lastQuater && phonePos.y > -phonePos.height / 2;
-    this.setState({
-      selectFocus,
-      explainFocus: !selectFocus && !phoneFocus,
-      phoneFocus: !selectFocus && phoneFocus,
-    });
-  }
 
-  onClickDivHandler() {
-    // scroller.scrollTo('reportSelect', {
-    //   duration: 500,
-    //   delay: 100,
-    //   smooth: true,
-    //   ignoreCancelEvents: true,
-    // });
-    animateScroll.scrollToTop({
-      duration: 500,
-      delay: 100,
-      smooth: true,
-      ignoreCancelEvents: true,
-    });
-  }
+    let newFocus = EXPLAIN;
+    if (selectPos.y > -selectPos.height / 4) {
+      newFocus = SELECT;
+    } else if (phonePos.y < lastQuater && phonePos.y > -phonePos.height / 2) {
+      newFocus = PHONE;
+    }
 
-  onClickBoxHandler() {
-    scroller.scrollTo('reportExplain', {
-      duration: 500,
-      delay: 100,
-      smooth: true,
-      offset: -half,
-      ignoreCancelEvents: true,
-    });
-  }
-
-  onClickPhoneHandler() {
-    scroller.scrollTo('reportPhone', {
-      duration: 500,
-      delay: 100,
-      smooth: true,
-      offset: -half,
-      ignoreCancelEvents: true,
-    });
+    if (focus !== newFocus) {
+      this.setState({ focus: newFocus });
+    }
   }
 
   render() {
+    const { focus, reportType, phoneNumber } = this.state;
+    const reportOptions = [
+      { id: 1, text: '사건을 제보할래요' },
+      { id: 2, text: '상담을 받고 싶어요' },
+      { id: 3, text: '대화 상대가 필요해요' },
+    ];
+
     return (
-      <div style={styles.divStyle}>
-        <Column xs="1" sm="1" md="1" lg="1" />
-        <Column xs="10" sm="10" md="10" lg="10">
-          <Element name="reportSelect">
-            <ReportSelect
-              focus={this.state.selectFocus}
-              onClickHandler={this.onClickBoxHandler}
-              selectRef={(node) => { if (node) this.selectNode = node; }}
-              onClickDivHandler={this.onClickDivHandler}
+      <div style={styles.scrollContainer}>
+        <Section
+          backgroundColor={colors.lightBlue}
+          style={styles.topSection}
+        >
+          <Row style={styles.centered}>
+            <Header
+              text="약간의 용기로 삶을 바꿀 수 있습니다."
+              color={colors.black}
+              size={2.5}
+              centered
             />
-          </Element>
-          <Element name="reportExplain">
-            <ReportExplain
-              focus={this.state.explainFocus}
-              onClickHandler={() => this.onClickBoxHandler()}
-              explainRef={(node) => { if (node) this.explainNode = node; }}
-              onClickDivHandler={this.onClickBoxHandler}
-              onNext={this.onClickPhoneHandler}
-            />
-          </Element>
-          <Element name="reportPhone">
-            <ReportPhone
-              focus={this.state.phoneFocus}
-              onSubmit={() => this.onSubmit()}
-              phoneRef={(node) => { if (node) this.phoneNode = node; }}
-              onClickDivHandler={this.onClickPhoneHandler}
-            />
-          </Element>
-          <ReportDone />
-        </Column>
-        <Column xs="1" sm="1" md="1" lg="1" />
+          </Row>
+        </Section>
+        <Element
+          name="select"
+          style={{ ...focus !== SELECT && styles.unfocused }}
+          onClick={() => scrollToElement(SELECT)}
+        >
+          <Section
+            style={styles.section}
+            reference={(node) => { if (node) this.selectNode = node; }}
+          >
+            <Row style={styles.centered}>
+              <Header text="어떤 도움이 필요하신가요?" />
+              <div style={styles.container}>
+                {reportOptions.map(option => (
+                  <Column xs={12} sm={4} md={4} lg={4} key={option.id}>
+                    <button
+                      style={{ ...styles.option, ...reportType === option.id && styles.selected }}
+                      onClick={event => this.onSelectOption(event, option.id)}
+                    >
+                      <h1 style={styles.optionText}>{option.text}</h1>
+                    </button>
+                  </Column>
+                ))}
+              </div>
+            </Row>
+          </Section>
+        </Element>
+        <Element
+          name={EXPLAIN}
+          style={{ ...focus !== EXPLAIN && styles.unfocused }}
+          onClick={() => scrollToElement(EXPLAIN)}
+        >
+          <Section
+            style={styles.section}
+            reference={(node) => { if (node) this.explainNode = node; }}
+          >
+            <Row>
+              <Header text="일어난 일에 대해 자세히 설명해 주세요." />
+              <div style={styles.container}>
+                <Textarea
+                  style={styles.textarea}
+                  minRows={4}
+                  placeholder="여기에 응답을 작성합니다."
+                />
+              </div>
+            </Row>
+          </Section>
+        </Element>
+        <Element
+          name="phone"
+          style={{ ...focus !== PHONE && styles.unfocused }}
+          onClick={() => scrollToElement(PHONE)}
+        >
+          <Section
+            style={styles.section}
+            reference={(node) => { if (node) this.phoneNode = node; }}
+          >
+            <Row>
+              <Header text="문제 해결을 위해 연락처를 여쭤 봐도 될까요?" />
+              <p style={styles.paragraph}>
+                부담이 된다면 남기지 않으셔도 됩니다.
+              </p>
+              <div style={styles.container}>
+                <input
+                  type="tel"
+                  style={styles.phoneInput}
+                  onChange={event => this.setState({ phoneNumber: event.target.value.replace(/-/g, '') })}
+                  value={formatPhoneNumber(phoneNumber)}
+                />
+              </div>
+            </Row>
+          </Section>
+        </Element>
+        <Section style={{ paddingTop: 0 }}>
+          <Row style={styles.centered}>
+            <Button
+              color={colors.kaistBlue}
+              style={{ cursor: 'pointer' }}
+              onClick={() => this.onSubmit()}
+            >
+              제출
+            </Button>
+          </Row>
+        </Section>
       </div>
     );
   }
--- client/src/static/constants.js
+++ client/src/static/constants.js
@@ -1,7 +1,9 @@
 const colors = {
   white: '#FFFFFF',
   black: '#000000',
+  gray: '#CCCCCC',
   darkGray: '#1B1B1B',
+  lightGray: '#EEEEEE',
   kaistBlue: '#004191',
   lightBlue: '#F3F9FC',
 };
